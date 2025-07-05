965d00028ca3851fe9e2d23ff8f09a2af2978515
arjunmaurya96 <arjunmaurya9621192312@gmail.com>
2025-07-05 16:57:24 +0530

    backend images url changes

--- backend/routes/Uploadimg.js
+++ backend/routes/Uploadimg.js
@@ -4,12 +4,14 @@ const multerUpload = require('../middleware/Multer')();
 
 const router = require("express").Router();
 
-router.post('/save', multerUpload?.single('file'),fileController)
+router.post('/save', multerUpload?.single('file'), fileController)
 
-function fileController(req, res){
-`http://localhost:8080/${req.file.filename}`
+function fileController(req, res) {
+    // `http://localhost:8080/${req.file.filename}`
+    `https://employee-manage-q8om.onrender.com/${req.file.filename}`
 
-res.send(`http://localhost:8080/${req.file.filename}`)
+    // res.send(`http://localhost:8080/${req.file.filename}`)
+    res.send(`https://employee-manage-q8om.onrender.com/${req.file.filename}`)
 
 
 }
